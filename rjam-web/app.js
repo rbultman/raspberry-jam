00176becfe343ad0439903e29237da4f3eb81be7
Rob Bultman <rob@firstbuild.com>
2020-08-18 20:27:26 -0400

    Starting on webapp session support

--- rjam-web/app.js
+++ rjam-web/app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
+app.use('/config', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
